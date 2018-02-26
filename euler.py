d7918c879ba0915a15177d0aa3a16955a6e8675e
Student of ISU <lab@irnok.net>
2018-02-26 13:13:15 +0800

    Added saving data to a Sqlite3 database.

--- euler.py
+++ euler.py
@@ -2,6 +2,7 @@
 
 import numpy
 import matplotlib.pyplot as plt
+import sqlite3 as sql
 
 def cooling_diffeq(k, Tenv):
     def f(t, T):
@@ -38,7 +39,7 @@ def draw(sol):
     plt.show()
 
 
-if __name__=="__main__":
+def test():
     sol = solve(f=cooling_diffeq(k=0.1, Tenv=24),
                 F0=100,
                 eps=0.01,
@@ -46,3 +47,85 @@ if __name__=="__main__":
                 n=10
                 )
     draw(sol)
+
+#test()
+
+def with_file(filename):
+    i = open(filename, "r")
+    
+    k = float(i.readline().strip())
+    Tenv = float(i.readline().strip())
+    F0 = float(i.readline().strip())
+    eps = float(i.readline().strip())
+    h = float(i.readline().strip())
+    n = int(i.readline().strip())
+    
+    i.close()
+    
+    sol = solve(f=cooling_diffeq(k=k, Tenv=Tenv),
+                F0=F0,
+                eps=eps,
+                h=h,
+                n=n
+    )
+
+    o=open("output.txt","w")
+    
+    for point in sol:
+        o.write("{}\n".format(point))
+
+    o.close()  # o.flush()
+
+    to_sqldb(sol)
+        
+    # draw(sol)
+
+def to_sqldb(result):
+    conn = sql.connect('example.db')
+    cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS trajectory")
+    conn.commit()
+    cursor.execute('''
+       CREATE TABLE trajectory
+         (value float)
+    ''')
+
+    for r in result:
+        cursor.execute('''
+           INSERT INTO trajectory (value)
+             VALUES
+           (?)''', (r,)
+        )
+    cursor.close()
+    conn.commit()
+    conn.close()
+    
+
+def consume(temp):
+    conn = sql.connect('example.db')
+    cursor = conn.cursor()
+
+
+    cursor.execute("SELECT value FROM trajectory WHERE value < ?", (temp,))
+
+    for row in cursor:
+        print(row)
+
+    cursor.execute("SELECT sum(value) FROM trajectory WHERE value<?", (temp,))
+    row = cursor.fetchone()
+
+    print("Sum is: ", row[0])
+
+    del cursor
+    conn.close()
+
+
+with_file('input.txt')
+
+
+consume(100)
+
+
+
+
+
