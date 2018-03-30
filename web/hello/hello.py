4ff5d7a33b3e3baf6c5391f391beebf15ba44230
ISU Student <labs@irnok.net>
2018-03-30 13:12:46 +0800

    A stub of a form has been created.

--- web/hello/hello.py
+++ web/hello/hello.py
@@ -17,8 +17,10 @@ print("CSV filename: ", filename)
 
 
 class HelloView(object):
-    def __init__(self, title="Hi!"):
+    def __init__(self, request=None, title="Hi!", number=None):
         self.title = title
+        self.number = int(number)  # FIXME: Check integer type
+        self.request = request
 
     def get_now(self):
         return datetime.now().strftime("%b %d %Y %H:%M:%S")
@@ -30,23 +32,65 @@ class HelloView(object):
                               delimiter=";",
                               quoting=csv.QUOTE_NONE
                               )
+
         next(csv_data)
-        return csv_data
+
+        if number is None:
+            def numbered_data(csv_data):
+
+                number = 1
+                for row in csv_data:
+                    yield row+[number]
+                    number += 1
+        else:
+            num = number
+            while num > 1:
+                next(csv_data)
+                num -= 1
+
+            def numbered_data(csv_data):
+
+                yield next(csv_data)+[number]
+
+        return numbered_data(csv_data)
 
     data = property(get_data)
 
+    def __call__(self, **params):
+        if self.request is None:
+            raise RuntimeError("this method cannot be tested")
+
+        answer = {"view": self, "request": self.request}
+        answer.update(params)
+        return answer
+
 
 def hello_world(request):
-    view = HelloView(title="Hi again!")
-    print("I'm here!")
-    return {"view": view, "request": request}
+    view = HelloView(request=request,
+                     title="Hi again!")
+    # print("I'm here!")
+
+    return view(copy="Students of ISU, 2018")
+
+
+def view_row(request):
+    view = HelloView(request=request,
+                     title="Edit record",
+                     number=request.GET.get("row"))
+    return view(subtitle="An extraordinary employee of our university")
 
 
 def main(global_config, **settings):
     with Configurator(settings=settings) as config:
+
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello',
                         renderer='hello:templates/index.pt')
+
+        config.add_route('view_emp', '/view')
+        config.add_view(view_row, route_name='view_emp',
+                        renderer='hello:templates/view.pt')
+
         config.add_static_view(name='css', path='hello:templates/css')
         config.add_static_view(
             name='fonts', path='hello:templates/fonts')
